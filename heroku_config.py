887c8df512c9f58da966acd058260818002da8e0
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-10-27 11:57:15 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -7,6 +7,7 @@ class Var(object):
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", None)
+    CUSTOM_PMPERMIT = OS.environ.get("CUSTOM_PMPERMIT")
     LOGGER = True
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
