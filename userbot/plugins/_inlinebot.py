4508fe3df2833d763452461da2a81899ad1ef791
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-10-31 11:21:32 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Check Pinned Message in\n@TechnoAyanBot And\nGet Your Own Userbot"
+            reply_pop_up_alert = "Check Pinned Message in\n@Godhackerzuserbot And\nGet Your Own Userbot"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
