86d8ff4291e5d1afa4460fcd64e2ed03a2c933ff
Abir Hasan <hackitabirhasan@gmail.com>
2020-11-03 20:05:51 +0600

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -29,7 +29,7 @@ PM_WARNS = {}
 PREV_REPLY_MESSAGE = {}
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 USER_BOT_WARN_ZERO = "__😡 Why Are You Spamming \n🤬 Stop This Shit Now \n😈 Else You'll Get Blocked__ "
-USER_BOT_NO_WARN = f"__👋 Hello There !\n\n🤖 I'm assistant of__ {DEFAULTUSER} __Sir\n\n💻 My Master Is Currently Busy\n\n✉️ send `/start` to Choose what you have came for \n\n🥳 You'll Get a Reply ASAP if you send `/start` **YOU NIGGA** \n\n🤨 Don't Spam Else Get Blocked \n\n❤️ Join My USERBOT SUPPORT GROUP [GODHACKERZ_USERBOT](https://t.me/Godhackerzuserbot)\n\n⚡️ Powered by__ [👩‍💻GODHACKERZ_USERBOT👨‍💻](https://github.com/rohithaditya/Godhackerz-userbot.git) /n/n**{Custom}**"
+USER_BOT_NO_WARN = f"__👋 Hello There !\n\n🤖 I'm assistant of__ {DEFAULTUSER} __Sir\n\n💻 My Master Is Currently Busy\n\n✉️ send `/start` to Choose what you have came for \n\n🥳 You'll Get a Reply ASAP if you send `/start` **YOU NIGGA** \n\n🤨 Don't Spam Else Get Blocked \n\n❤️ Join My USERBOT SUPPORT GROUP [GODHACKERZ_USERBOT](https://t.me/Godhackerzuserbot)\n\n⚡️ Powered by__ [👩‍💻GODHACKERZ_USERBOT👨‍💻](https://github.com/rohithaditya/Godhackerz-userbot.git) \n\n**{Custom}**"
 
 
 if Var.PRIVATE_GROUP_ID is not None:
