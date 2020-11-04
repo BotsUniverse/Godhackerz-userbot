0d9d9c55be0f8f520f46400ad8e11f5ea7a5269c
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-11-04 12:44:37 +0530

    Ai

--- userbot/_core.py
+++ userbot/_core.py
@@ -14,7 +14,7 @@ from datetime import datetime
 
 DELETE_TIMEOUT = 5
 
-@command(pattern="^.install", outgoing=True)
+@command(pattern="^.add", outgoing=True)
 async def install(event):
     if event.fwd_from:
         return
@@ -28,10 +28,10 @@ async def install(event):
                 path1 = Path(downloaded_file_name)
                 shortname = path1.stem
                 load_module(shortname.replace(".py", ""))
-                await event.edit("Installed Plugin `{}`".format(os.path.basename(downloaded_file_name)))
+                await event.edit("Installed Plugin `{}` Master".format(os.path.basename(downloaded_file_name)))
             else:
                 os.remove(downloaded_file_name)
-                await event.edit("Errors! Cannot install this plugin.")
+                await event.edit("Error Master Cannot install this plugin. Maybe preloaded or already Installed Master. If you find any errors plz report at  @Godhackerzuserbot") 
         except Exception as e:  # pylint:disable=C0103,W0703
             await event.edit(str(e))
             os.remove(downloaded_file_name)
@@ -55,7 +55,7 @@ async def send(event):
     )
     end = datetime.now()
     time_taken_in_ms = (end - start).seconds
-    await event.edit("Uploaded {} in {} seconds".format(input_str, time_taken_in_ms))
+    await event.edit("Uploaded {} in {} seconds Master".format(input_str, time_taken_in_ms))
     await asyncio.sleep(DELETE_TIMEOUT)
     await event.delete()
 
@@ -68,7 +68,7 @@ async def unload(event):
         remove_plugin(shortname)
         await event.edit(f"Unloaded {shortname} successfully")
     except Exception as e:
-        await event.edit("Successfully unload {shortname}\n{}".format(shortname, str(e)))
+        await event.edit("Successfully unload {shortname}\n{} Plugin Master".format(shortname, str(e)))
 
 @command(pattern="^.load (?P<shortname>\w+)$", outgoing=True)
 async def load(event):
@@ -81,6 +81,6 @@ async def load(event):
         except:
             pass
         load_module(shortname)
-        await event.edit(f"Successfully loaded {shortname}")
+        await event.edit(f"Successfully loaded {shortname}This Plugin Master")
     except Exception as e:
-        await event.edit(f"Could not load {shortname} because of the following error.\n{str(e)}")
+        await event.edit(f"Could not load {shortname} because of the following error. Master\n{str(e)}")
