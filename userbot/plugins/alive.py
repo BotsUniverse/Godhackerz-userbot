8227256cc23148461e812ac8cb510a4fc646b334
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-11-04 13:51:13 +0530

    Full AI

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,7 +9,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/8e929112328df851306e4.jpg"
 pm_caption = "**This is GodHackerz Userbot**\n\n"
 
-pm_caption += "Hi THERE {DEFAULTUSER} MASTER ! I am Alive. All functions are working properly.\n\n"
+pm_caption += "Hi THERE { DEFAULTUSER } MASTER ! I am Alive. All functions are working properly.\n\n"
 pm_caption += "⚡️Status⚡️\n\n"
 pm_caption += "😎Telethon Version : (1.16.04)\n"
 pm_caption += "🥳Python : (4.0)\n"
