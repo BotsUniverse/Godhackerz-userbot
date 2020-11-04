22ce25540b4b2502b9c854ddd65de51f3415693f
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-11-04 11:13:51 +0530

    Added A. I

--- userbot/plugins/get_id.py
+++ userbot/plugins/get_id.py
@@ -1,11 +1,12 @@
 """Get ID of any Telegram media, or any user
-Syntax: .get_id"""
+Syntax: .chatid
+© @Godhackerzuserbot"""
 from telethon import events
 from telethon.utils import pack_bot_file_id
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("get_id"))
+@borg.on(admin_cmd("chatid"))
 async def _(event):
     if event.fwd_from:
         return
@@ -14,8 +15,8 @@ async def _(event):
         r_msg = await event.get_reply_message()
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(str(event.chat_id), str(r_msg.from_id), bot_api_file_id))
+            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(str(event.chat_id), str(r_msg.sender_id), bot_api_file_id))
         else:
-            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`".format(str(event.chat_id), str(r_msg.from_id)))
+            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`".format(str(event.chat_id), str(r_msg.sender_id)))
     else:
         await event.edit("Current Chat ID: `{}`".format(str(event.chat_id)))
