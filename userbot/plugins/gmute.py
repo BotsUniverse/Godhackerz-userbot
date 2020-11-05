e8d9999971bf50dbeee1bd10e4bc69e539465d21
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-11-05 10:03:44 +0530

    changes

--- userbot/plugins/gmute.py
+++ userbot/plugins/gmute.py
@@ -3,9 +3,9 @@ G-Muter Plugin for userbot. //Needs MongoDB to work.
 cmds: .gmute user_id|reply to user messsage	//G-Mutes a User.
 	  .ungmute user_id|reply to user messsage //Un-Gmutes a User.
 	  .listgmuted //List Currently G-Muted Users.
-
 By:- JaskaranSM ( @Zero_cool7870 )
-For © @Godhackerzuserbot
+© For @Godhackerzuserbot
+Ask @Zero_cool7870 Tg Before Editing Or Copying 
 """
 
 from telethon import events
@@ -13,7 +13,11 @@ import logging
 import asyncio
 from uniborg.util import admin_cmd
 logging.basicConfig(level=logging.INFO)
-MONGO_URI = Config.MONGO_URI
+
+
+MONGO_URI= Config.MONGO_URI
+if MONGO_URI is None:
+	logging.error("ADD MONGO_URI in Env Vars Plox.")
 try:	
 	db = mongo_client['test']
 	muted = db.muted
@@ -26,61 +30,61 @@ async def gmute_user(event):
 		return
 	input_str = event.pattern_match.group(1)	
 	if not event.reply_to_msg_id and not input_str:
-		await event.edit("`Master Give Me User id or Reply To a User Message To Mute.`")
+		await event.edit("`Give a User id or Reply To a User Message To Mute.`")
 		return	
 	if event.reply_to_msg_id:
 		msg = await event.get_reply_message()
-		user_id = msg.sender_id	
+		user_id = msg.from_id	
 	else:
 		user_id = int(input_str)
-                
-	await event.edit("`Master Give username or userid or replay to usermsg master`")	
+            if user_id == 1207066133:
+        return
+	await event.edit("`Getting a duct tape..`")	
 	try:
 		chat = await event.get_chat()
 		is_admin = chat.admin_rights
-		is_creator = "1207066133"
+		is_creator = chat.creator
 	except:
-		await event.edit("`You Need to Run this command in a Group Chat Master. `")	
+		await event.edit("`You Need to Run this command in a Group Chat.`")	
 		return
 	if is_admin or is_creator:	
 		try:
 			c = muted.find({})
 			for i in c:
 				if i['user_id'] == user_id:
-					await event.edit("`Master This Noob User is Already G-Muted.`")
+					await event.edit("`User is Already G-Muted.`")
 					return
-			        if user_id == is_creator:
-					await event.edit("`Cant Mute My Creator Master..`")
+			if user_id == borg.me.id:
+					await event.edit("`Cant Mute Myself..`")
 					return
 			else:
 				muted.insert_one({'user_id':user_id})
-				await event.edit("`Successfully GMuted And Forwarded to  @Godhackerzuserbotn` [{}](tg://user?id={}).".format(str(user_id),str(user_id)))
+				await event.edit("`G-Muted` [{}](tg://user?id={}).".format(str(user_id),str(user_id)))
 				logging.info("G-Muted {}".format(str(user_id)))
 		except Exception as e:
 			logging.error(str(e))
 			await event.edit("Error: "+str(e))
 			return
 	else:		
-		await event.edit("`You are not admin Here Master.`")
+		await event.edit("`You are not admin Here.`")	
 
-        
 @borg.on(admin_cmd(pattern="ungmute ?(.*)", allow_sudo=True))
 async def un_gmute_user(event):
 	if event.fwd_from:
 		return
 	input_str = event.pattern_match.group(1)	
 	if not event.reply_to_msg_id and not input_str:
-		await event.edit("`Give a User id or Reply To a User Message To Unmute Master`")
+		await event.edit("`Give a User id or Reply To a User Message To Mute.`")
 		return	
 	if event.reply_to_msg_id:
 		msg = await event.get_reply_message()
-		user_id = msg.sender_id	
+		user_id = msg.from_id	
 	else:
 		user_id = int(input_str)	
 	await event.edit("`Removing Duct Tape from User's Mouth.`")	
 	try:
 		muted.delete_one({'user_id':user_id})
-		await event.edit("`Un-Gmuted This Noob` [{}](tg://user?id={}).".format(str(user_id),str(user_id)))
+		await event.edit("`Un-Gmuted` [{}](tg://user?id={}).".format(str(user_id),str(user_id)))
 		logging.info("Un-Gmuted {}".format(str(user_id)))
 	except Exception as e:
 		logging.error(str(e))
@@ -107,7 +111,7 @@ async def gmute_listener(sender):
 	try:
 		curs = muted.find({})
 		for c in curs:
-			if c['user_id'] == sender.sender_id:
+			if c['user_id'] == sender.from_id:
 				await sender.delete()
 	except:
 		return 
