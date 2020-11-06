f39b1ccbfefc6d7040c94abba0c368adbc3a0a74
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-11-06 13:20:01 +0530

    Update Stickers_v2.py

--- userbot/plugins/Stickers_v2.py
+++ userbot/plugins/Stickers_v2.py
@@ -51,7 +51,7 @@ async def _(event):
     me = borg.me
     userid = event.sender_id
     packname = f"{STICKER_PACK_NAME}"
-    packshortname = f"{DEFAULTUSER}Godhackerzuserbot"  # format: Uni_Borg_userid
+    packshortname = f"{userid}_Godhackerzuserbot_{DEFAULTUSER}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@Godhackerzuserbot_stcr.png"
@@ -64,7 +64,7 @@ async def _(event):
         if userid == 1207066133:
             packshortname = "@Godhackerzuserbot_3d"
         else:
-            packshortname = f"{DEFAULTUSER}Godhackerzuserbot" # format: Uni_Borg_userid
+            packshortname = f"{userid}_Godhackerzuserbot_{DEFAULTUSER}" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
