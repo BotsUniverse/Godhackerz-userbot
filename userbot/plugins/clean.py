dd255626dff4624324be66e3bd867e5c8d5dfc8a
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-11-16 08:21:12 +0530

    Create clean.py

---
+++ userbot/plugins/clean.py
@@ -0,0 +1,129 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# lots of luv to MrConfused To Add Sudo in it and fix
+# Thanks To Friday For This Plugin 
+# © @Godhackerzuserbot
+
+"""Cmd= `.clean`
+Usage: Searches for deleted accounts in a groups and channels.
+Use .clean clean to remove deleted accounts from the groups and channels.
+\nPorted by ©[NIKITA](t.me/kirito6969) and ©[EYEPATCH](t.me/NeoMatrix90)"""
+
+import asyncio
+from asyncio import sleep
+
+from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
+
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd, sudo_cmd
+
+#
+BOTLOG = True
+BOTLOG_CHATID = Config.PRIVATE_GROUP_ID
+
+# =================== CONSTANT ===================
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+
+UNBAN_RIGHTS = ChatBannedRights(
+    until_date=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None,
+)
+
+
+@borg.on(admin_cmd(pattern=f"clean ?(.*)"))
+async def rm_deletedacc(show):
+    """ For .clear command, list all the ghost/deleted/zombie accounts in a chat. """
+
+    con = show.pattern_match.group(1).lower()
+    del_u = 0
+    del_status = "`No deleted accounts found, Group is clean Master `"
+
+    if con != "clean":
+        await show.edit("`Searching for ghost/deleted/zombie accounts...`")
+        async for user in show.client.iter_participants(show.chat_id):
+
+            if user.deleted:
+                del_u += 1
+                await sleep(1)
+        if del_u > 0:
+            del_status = f"`Found` **{del_u}** ghost/deleted/zombie account(s) in this group, Master \
+            \nclean them by using `.clean`"
+        await show.edit(del_status)
+        return
+
+    # Here laying the sanity check
+    chat = await show.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    # Well
+    if not admin and not creator:
+        await show.edit("` Master You Are not an admin here! if Any Error Go Check Support Group Master`")
+        return
+
+    await show.edit("`Deleting deleted accounts...\nOh I can do that Na Master !`")
+    del_u = 0
+    del_a = 0
+
+    async for user in show.client.iter_participants(show.chat_id):
+        if user.deleted:
+            try:
+                await show.client(
+                    EditBannedRequest(show.chat_id, user.id, BANNED_RIGHTS)
+                )
+            except ChatAdminRequiredError:
+                await show.edit("`Master don't have ban rights in this group 🤬🤬🤬😡`")
+                return
+            except UserAdminInvalidError:
+                del_u -= 1
+                del_a += 1
+            await show.client(EditBannedRequest(show.chat_id, user.id, UNBAN_RIGHTS))
+            del_u += 1
+
+    if del_u > 0:
+        del_status = f"Cleaned **{del_u}** deleted account(s)"
+
+    if del_a > 0:
+        del_status = f"Cleaned **{del_u}** deleted account(s) \
+        \n**{del_a}** deleted admin accounts are not removed Master"
+
+    await show.edit(del_status)
+    await sleep(2)
+    await show.delete()
+
+    if BOTLOG:
+        await show.client.send_message(
+            BOTLOG_CHATID,
+            "#CLEANUP\n"
+            f"Cleaned **{del_u}** deleted account(s)  Master !!\
+            \nCHAT: {show.chat.title}(`{show.chat_id}`)",
+        )
+
+CMD_HELP.update(
+    {
+        "zombies": ".zombies\
+\nUsage: Searches for deleted accounts in a group. Use .delusers clean to remove deleted accounts from the group.\
+"
+    }
+)
