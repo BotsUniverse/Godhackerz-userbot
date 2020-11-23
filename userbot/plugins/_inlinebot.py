fa10977f34753eec8197a632cc6443dd1f9a0791
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-11-23 10:28:00 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -18,7 +18,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
                 "© GodHackerz-UserBot Help",
-                text="{}\n🔢 Currently Loaded Plugins: {}".format(
+                text="{}\n😎Currently Master Has Loaded Plugins: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
                 link_preview=False
@@ -109,7 +109,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("<<Previous", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("Next>>", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("<--", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("-->", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
