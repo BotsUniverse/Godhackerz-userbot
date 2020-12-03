3df627ca766ca1f7285f38c5067f24f73dfe7a22
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-12-03 10:32:44 +0000

    Update heroku.py

--- userbot/plugins/heroku.py
+++ userbot/plugins/heroku.py
@@ -220,7 +220,7 @@ async def _(givelogs):
         givelogs.chat_id,
         "logs.txt",
         reply_to=givelogs.id,
-        caption="Logs Collected Using Heroku \n For More Support Visit [Userbot Support Group](https://t.me/Godhackerzuserbot)
+        caption="Logs Collected Using Heroku \n For More Support Visit [Userbot Support Group](https://t.me/Godhackerzuserbot)"
     )
     await edit_or_reply(givelogs, "`Logs Send Sucessfully ! `")
     await asyncio.sleep(5)
