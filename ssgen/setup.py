0c52b030915cee6a5d710a55ffaa7b9efcda61ac
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-12-05 18:29:43 +0530

    Update setup.py

--- ssgen/setup.py
+++ ssgen/setup.py
@@ -11,8 +11,8 @@ APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
-    Bot = client.send_message("me", client.session.save())
-   Bot.reply(
+    tele = client.send_message("me", client.session.save())
+   tele.reply(
         "The above is the `STRING_SESSION` for your current session.\nJoin @Godhackerzuserbot For Support\n (c) ")
     print("")
     print("Below is the STRING_SESSION. You can also find it in your Telegram Saved Messages.")
