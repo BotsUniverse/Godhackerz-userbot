8a0dbe5797fa61a9daf7bd0f9fee0b24a31cf1f0
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-12-07 12:39:48 +0530

    Create stringsessiongen.sh

---
+++ ssgen/stringsessiongen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "
+Termux session string generator for Godhackerz Userbot 
+"
+echo Starting dependency installation in 5 seconds...
+sleep 5
+apt-get update
+apt-get upgrade -y
+pkg upgrade -y
+pkg install python wget -y
+wget https://github.com/rohithaditya/Godhackerz-userbot/blob/main/ssgen/setup.py
+pip install telethon
+python setup.py
