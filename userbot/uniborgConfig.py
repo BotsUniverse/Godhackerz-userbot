91e824c049695e4ddaa10ec82f5710ef6d4b3b48
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-12-10 12:59:17 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -12,7 +12,7 @@ if ENV:
         SCREEN_SHOT_LAYER_ACCESS_KEY = os.environ.get("SCREEN_SHOT_LAYER_ACCESS_KEY", None)
         # Send .get_id in any group to fill this value.
         PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID", -100123456789))
-        PRIVATE_GROUP_ID = int(os.environ.get("PRIVATE_GROUP_ID", -100123456789))
+        PRIVATE_GROUP_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID", -100123456789))
         # Send .get_id in any channel to fill this value. ReQuired for @Godhackerzuserbot inspiration to work!
         PRIVATE_CHANNEL_BOT_API_ID = int(os.environ.get("PRIVATE_CHANNEL_BOT_API_ID", -100123456789))
         # This is required for the plugins involving the file system.
