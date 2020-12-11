af6326ab5f9709b6c7d5dcfd3a28045dee0458ee
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-12-11 11:08:19 +0000

    Update memes1.py

--- userbot/plugins/memes1.py
+++ userbot/plugins/memes1.py
@@ -38,3 +38,16 @@ async def mod(event):
     tap = await bot.inline_query(botusername, app) 
     await tap[0].click(event.chat_id)
     await event.delete()
+
+
+@borg.on(admin_cmd(pattern="vid"))
+async def mod(event):
+    if event.fwd_from:
+        return
+    music =event.pattern_match.group(1)
+    botusername = "@Vid"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, music)
+    await tap[0].click(event.chat_id)
+    await event.delete()
