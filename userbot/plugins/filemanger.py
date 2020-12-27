6026c44352617ff30212e7e0fc720531333fe8f9
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2020-12-27 19:40:29 +0530

    Update filemanger.py

--- userbot/plugins/filemanger.py
+++ userbot/plugins/filemanger.py
@@ -1,5 +1,7 @@
-v"""Execute GNU/Linux commands inside Telegram
-Syntax: .lsroot , .lslocal"""
+"""
+Execute GNU/Linux commands inside Telegram
+Syntax: .lsroot , .lslocal
+"""
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
